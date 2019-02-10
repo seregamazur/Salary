0bd3c63087c7b9e15be13de76cd4015c0caa7217
seregamazur <serhiy.mazur0@gmail.com>
2019-02-10 23:09:48 +0200

    Added sout for first and last employees

--- src/main/java/work/Start.java
+++ src/main/java/work/Start.java
@@ -10,19 +10,26 @@ public class Start implements Serializable {
     }
 
     private void start() throws IOException, ClassNotFoundException {
-        File serial = new File("src/main/java/work/set");
+        //creating new file where we would save out set
+        File serial = new File("src/main/java/work/set.data");
         ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(serial));
         List<BaseEmployee> employees = addEmployees();
+        //creating set
         NavigableSet<Object> database = new TreeSet<>().descendingSet();
         database.addAll(employees);
-        database.forEach(System.out::println);
         os.writeObject(database);
         os.flush();
         os.close();
         ObjectInputStream is = new ObjectInputStream(new FileInputStream(serial));
         Set<? extends BaseEmployee> emp = (Set<? extends BaseEmployee>) is.readObject();
-        emp.forEach(System.out::println);
         is.close();
+        //sout first five employees
+        emp.stream().limit(5).forEach(System.out::println);
+        //sout last 3 employees
+        emp.stream().skip(emp.size()-3).forEach(System.out::println);
+
+
+
     }
 
     private static List<BaseEmployee> addEmployees() {
@@ -31,12 +38,15 @@ public class Start implements Serializable {
         BaseEmployee employee2 = new HourRateEmployee(new EmployeeInfo(3, "Aleksa", "Mazur"), 10, 30);
         BaseEmployee employee4 = new HourRateEmployee(new EmployeeInfo(5, "Aaeksa", "Mazur"), 10, 30);
         BaseEmployee employee3 = new HourRateEmployee(new EmployeeInfo(4, "Bleksa", "Mazur"), 10, 30);
+        BaseEmployee employee5 = new HourRateEmployee(new EmployeeInfo(10, "Cleksa", "Mazur"), 10, 30);
         List<BaseEmployee> employees = new ArrayList<>();
         employees.add(employee1);
         employees.add(employee);
         employees.add(employee4);
         employees.add(employee2);
         employees.add(employee3);
+        employees.add(employee5);
+
         return employees;
     }
 
